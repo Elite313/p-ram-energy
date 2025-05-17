9e030dc2aeef764de2f8d034b742e00655d2e219
Elite313 <eliteforce077@gmail.com>
2025-05-17 18:18:51 +0530

    Remove sensitive information from server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('Connected to MongoDB at:', MONGODB_URI))
+.then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 // File Upload Configuration
